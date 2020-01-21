a3ff39fd40f39d5bc2afa86abfda00764fda6a90
Mash J Prime <mashjprime@gmail.com>
2020-01-21 22:58:41 +0800

    Added: Random Card chooser function

--- js/test.js
+++ js/test.js
@@ -39,9 +39,28 @@ function isPangram(sentence) {
   return true;
 }
 
-
 function firstCharToUpper(word) {
   // Retrieve first letter and convert to uppercase
   let firstChar = word[0].toUpperCase();
   return firstChar + word.slice(1);
+}
+
+let temp = {
+  name: "bob",
+  age: 30
+};
+console.log(temp);
+
+function getCard() {
+  const value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', "J", "Q", "K", "A"];
+  const suit = ["Clubs", "Spades", "Diamonds", "Hearts"];
+  return {
+    value: randomArray(value),
+    suit: randomArray(suit)
+  };
+}
+
+function randomArray(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
